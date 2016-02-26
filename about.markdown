acd548134f693eb8a18e1af815fba206fdfd7229
Kia <sadieperkins@riseup.net>
2016-02-25 22:11:23 -0800

    about

---
+++ about.markdown
@@ -0,0 +1,8 @@
+---
+title: About
+---
+
+github: [matildah](https://github.com/matildah/)
+
+
+email: rot13(fnqvrcrexvaf@evfrhc.arg)
--- site.hs
+++ site.hs
@@ -22,6 +22,13 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
+	match (fromList ["about.markdown"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+
     match "posts/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
